dc1aa2d24853c7f79e219b0fea6c9ae7d9f3383a
LenaQA94 <lenaqa94@gmail.com>
2025-08-02 13:20:29 +0300

    Add files via upload

--- 3.py
+++ 3.py
@@ -1,16 +1,12 @@
-# Есть массив строк. Все строки содержат одинаковые буквы , кроме одной. Попробуйте найти её!
-
-from collections import Counter
-
-
-def find_uniq(arr):
-    def norm(s):
-        return ''.join(sorted(set(s.replace(' ', '').lower())))
-
-    normalized = list(map(norm, arr))
-    freq = Counter(normalized)
-    unique_key = next(k for k, v in freq.items() if v == 1)
-    return arr[normalized.index(unique_key)]
+# Напишите класс Block, который создает блок (ну да...)
+#
+# Требования
+# Конструктор должен принимать в качестве аргумента массив,
+# содержащий 3 целых числа формы, [width, length, height]из которой Blockдолжен быть создан.
 
 
-print(find_uniq(['Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a']))
+class Block:
+    # Good Luck!
+
+
+print(initials('Barack hussein obama'))
