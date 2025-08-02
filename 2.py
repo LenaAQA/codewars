978381f2c5afbe413b090b4af2413f6826f969e8
LenaQA94 <lenaqa94@gmail.com>
2025-08-02 13:20:08 +0300

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,16 +1,25 @@
-# Есть массив с несколькими числами. Все числа равны, кроме одного. Попробуйте найти его!
+# Теперь, когда у нас есть блок , давайте перейдем к чему-то более сложному:
+# Sphere.
+#
+# Аргументы в пользу конструктора
+# radius -> integer or float (do not round it)
+# mass -> integer or float (do not round it)
+# Методы, которые необходимо определить
+# get_radius()       =>  radius of the Sphere (do not round it)
+# get_mass()         =>  mass of the Sphere (do not round it)
+# get_volume()       =>  volume of the Sphere (rounded to 5 place after the decimal)
+# get_surface_area() =>  surface area of the Sphere (rounded to 5 place after the decimal)
+# get_density()      =>  density of the Sphere (rounded to 5 place after the decimal)
+# Пример
+# ball = Sphere(2,50)
+# ball.get_radius() ->       2
+# ball.get_mass() ->         50
+# ball.get_volume() ->       33.51032
+# ball.get_surface_area() -> 50.26548
+# ball.get_density() ->      1.49208
+
+class Sphere(object):
+    pass
 
 
-# def find_uniq(arr):
-#     arr = sorted(arr)
-#     if arr[0] == arr[1]:
-#         return arr[-1]
-#     else:
-#         return arr[0]
-
-def find_uniq(arr):
-    arr = sorted(arr)
-    return arr[-1] if arr[0] == arr[1] else arr[0]
-
-
-print(find_uniq([1, 1, 1, 2, 1, 1]))
+print(initials('Barack hussein obama'))
