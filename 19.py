d220f480347aad28e68ef1ddd709963a9139849d
AQA <lenaqa94@gmail.com>
2025-08-06 14:12:24 +0300

    Delete 19.py

--- 19.py
+++
@@ -1,8 +0,0 @@
-# Ваша задача — просто подсчитать общее количество строчных букв в строке.
-
-
-def lowercase_count(strng):
-    return len([i for i in strng if i.islower()])
-
-
-print(lowercase_count("abcABC123"))
