483d775dc4bf94b256373b0817dbc1c246311b90
AQA <lenaqa94@gmail.com>
2025-08-06 14:11:35 +0300

    Delete 18.py

--- 18.py
+++
@@ -1,8 +0,0 @@
-# Число перепутано с текстом. Ваша цель — восстановить число из текста
-
-
-def filter_string(st):
-    return int("".join([i for i in st if i.isdigit()]))
-
-
-print(filter_string("aa1bb2cc3dd"))
